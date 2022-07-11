ca2ac7ba0d00ab39ee7c8b76ed18d8ec8f6378b1
Alejandro Montes <ango19941994@gmail.com>
2022-07-11 18:56:54 +0200

    Add router options and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Home } from './pages/home';
+import { iRouterItem } from './interfaces/iRouterItem';
 import { loadArtistsAction } from './reducers/artists/artists.action.creators';
 import { loadComicsAction } from './reducers/comics/comics.action.creators';
 import { ArtistHttpStore } from './services/artist.http.store';
@@ -21,14 +21,49 @@ function App() {
             .getAllComics()
             .then((comics) => dispatcher(loadComicsAction(comics)));
     }, [apiArtsits, apiComics, dispatcher]);
-    <Routes>
-        <Route path="/casa" element={<Home></Home>}></Route>
-    </Routes>;
+    const HomePage = React.lazy(() => import('./pages/home'));
+    const CategoriesPage = React.lazy(() => import('./pages/categories'));
+    const ArtistsPage = React.lazy(() => import('./pages/artists'));
+    const MyComixPage = React.lazy(() => import('./pages/mycomix'));
+    const LoginPage = React.lazy(() => import('./pages/login'));
+    const RegisterPage = React.lazy(() => import('./pages/register'));
+
+    const routerOptions: Array<iRouterItem> = [
+        { path: '/', label: 'Home', page: <HomePage></HomePage> },
+        {
+            path: '/categories',
+            label: 'Categories',
+            page: <CategoriesPage></CategoriesPage>,
+        },
+        {
+            path: '/artists',
+            label: 'Artists',
+            page: <ArtistsPage></ArtistsPage>,
+        },
+        {
+            path: '/mycomix',
+            label: 'MyComix',
+            page: <MyComixPage></MyComixPage>,
+        },
+        { path: '/login', label: 'Login', page: <LoginPage></LoginPage> },
+        {
+            path: '/register',
+            label: 'Register',
+            page: <RegisterPage></RegisterPage>,
+        },
+    ];
     return (
-        <div className="App">
-            <Link to="/casa">El pepe</Link>
-            <h1>Hola mundo</h1>;
-        </div>
+        <React.Suspense>
+            <Routes>
+                {routerOptions.map((item) => (
+                    <Route
+                        key={item.label}
+                        path={item.path}
+                        element={item.page}
+                    ></Route>
+                ))}
+            </Routes>
+        </React.Suspense>
     );
 }
 
