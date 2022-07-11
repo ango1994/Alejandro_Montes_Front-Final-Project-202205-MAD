e0c8bee1b40ed16b806fa82c911b9b4a27a51327
Alejandro Montes <ango19941994@gmail.com>
2022-07-11 12:26:30 +0200

    Add artists reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,18 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { artistsReducer } from '../reducers/artists/artists.reducer';
 
 export const store = configureStore({
-  reducer: {},
+    reducer: {
+        artist: artistsReducer,
+    },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
 >;
+
