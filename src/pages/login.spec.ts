8443878ebe8b01475fec2426c09f61839c5a5c37
Alejandro Montes <ango19941994@gmail.com>
2022-07-12 16:20:05 +0200

    Add test utils

---
+++ src/pages/login.spec.ts
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+
+describe('Given the Login component', () => {
+    describe('When it is called', () => {
+        test('Then it shoul render the login page', () => {});
+    });
+});
