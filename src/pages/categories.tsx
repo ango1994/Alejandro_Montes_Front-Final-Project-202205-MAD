05960b72c29ebe4986afead1c1452bd971e45b5a
Alejandro Montes <ango19941994@gmail.com>
2022-07-13 20:28:10 +0200

    Add register testing

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -1,7 +1,37 @@
 export function Categories() {
     return (
         <div>
-            <h1>Elpepe</h1>
+            <div>
+                <p>categories</p>
+                <p>categories</p>
+                <p>categories</p>
+            </div>
+            <div>
+                <div>
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/comix-46ded.appspot.com/o/Portadas%2Feuropeo.png?alt=media&token=b4eba0e8-8df7-4ac0-b07d-72b59b18e06f"
+                        alt=""
+                    />
+                    <h3>EUROPEAN</h3>
+                </div>
+                <div>
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/comix-46ded.appspot.com/o/Portadas%2Famericano.png?alt=media&token=42cb296f-863b-4856-bd49-9390de53866b"
+                        alt=""
+                    />
+                    <h3>AMERICAN</h3>
+                </div>
+                <div>
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/comix-46ded.appspot.com/o/Portadas%2Fmanga.png?alt=media&token=46cd438c-e57b-457b-88ce-24110e310a90"
+                        alt=""
+                    />
+                    <h3>MANGA</h3>
+                </div>
+
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
         </div>
     );
 }
