09b9dde14464f17c0ee806d317df6b6a86632db0
Alejandro Montes <ango19941994@gmail.com>
2022-07-14 20:20:00 +0200

    Add category testing

---
+++ src/components/category.spec.tsx
@@ -0,0 +1,40 @@
+import { BrowserRouter } from 'react-router-dom';
+import { iArtist } from '../interfaces/iArtist';
+import { iComic } from '../interfaces/iComics';
+import { userWithToken } from '../interfaces/iUser';
+import { artistsReducer } from '../reducers/artists/artists.reducer';
+import { comicsReducer } from '../reducers/comics/comics.reducer';
+import { usersReducer } from '../reducers/users/users.reducer';
+import { iStore } from '../store/store';
+import { render, screen } from '../utils/test.utils';
+import Category from './category';
+
+const reducer = {
+    comics: comicsReducer,
+    artists: artistsReducer,
+    user: usersReducer,
+};
+
+const preloadedState: iStore = {
+    comics: [] as Array<iComic>,
+    artists: [] as Array<iArtist>,
+    user: {} as userWithToken,
+};
+
+describe('Given the component Header', () => {
+    describe('When it is called', () => {
+        test('Then it should render the header', () => {
+            render(
+                <BrowserRouter>
+                    <Category name="test" img=""></Category>
+                </BrowserRouter>,
+                {
+                    preloadedState,
+                    reducer,
+                }
+            );
+            const element = screen.getByText(/test/i);
+            expect(element).toBeInTheDocument();
+        });
+    });
+});
