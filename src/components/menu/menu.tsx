6ff52b1101e4d600292cb8b11d2edf8df778df39
Alejandro Montes <ango19941994@gmail.com>
2022-07-18 20:17:47 +0200

    update menu component

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './menu.module.css';
 
 export function Menu({ menu }: { menu: Function }) {
@@ -6,8 +7,52 @@ export function Menu({ menu }: { menu: Function }) {
     };
     return (
         <div className={styles.menu}>
-            <h1>Menu</h1>
-            <button onClick={handleClick}>CLOSE</button>
+            <nav className={styles.navigation}>
+                <p className={styles.small}>01</p>
+                <Link
+                    to={'/home'}
+                    className={styles.navItem}
+                    onClick={handleClick}
+                >
+                    HOME
+                </Link>
+                <p className={styles.small}>02</p>
+                <Link
+                    to={'/artists'}
+                    className={styles.navItem}
+                    onClick={handleClick}
+                >
+                    ARTISTS
+                </Link>
+                <p className={styles.small}>03</p>
+                <Link
+                    to={'/categories'}
+                    className={styles.navItem}
+                    onClick={handleClick}
+                >
+                    CATEGORIES
+                </Link>
+                <p className={styles.small}>04</p>
+                <Link
+                    to={'/mycomix'}
+                    className={styles.navItem}
+                    onClick={handleClick}
+                >
+                    MYCOMIX
+                </Link>
+            </nav>
+            <div className={styles.menuRight}>
+                <button onClick={handleClick} className={styles.close}>
+                    close
+                </button>
+                <Link
+                    to={'/login'}
+                    className={styles.login}
+                    onClick={handleClick}
+                >
+                    login
+                </Link>
+            </div>
         </div>
     );
 }
