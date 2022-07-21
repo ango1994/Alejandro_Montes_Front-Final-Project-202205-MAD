059f4588d4d27003c6588f961e2cfe4c14e63d20
Alejandro Montes <ango19941994@gmail.com>
2022-07-21 19:36:34 +0200

    Add home styling

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -13,6 +13,23 @@ export function Home() {
                     The right <span className={styles.bold}>place</span> to
                     manage your <span className={styles.bold}>comics</span>
                 </h2>
+                <div className={styles.carousel}>
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/comix-46ded.appspot.com/o/Comics%2FAMERICANO%2FMesa%20de%20trabajo%201.webp?alt=media&token=0215c4e7-24d2-41be-bb32-8b1abccbd2d6"
+                        alt=""
+                        className={styles.car}
+                    />
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/comix-46ded.appspot.com/o/Comics%2FMANGA%2FAKIRA.webp?alt=media&token=cc8e7790-149d-4fa3-be4d-34ab4319a632"
+                        alt=""
+                        className={styles.car}
+                    />
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/comix-46ded.appspot.com/o/Comics%2FMANGA%2FBerserk2Short.webp?alt=media&token=a7da703c-1db0-4af2-b7ae-c867bec3d44f"
+                        alt=""
+                        className={styles.car}
+                    />
+                </div>
             </div>
         </div>
     );
